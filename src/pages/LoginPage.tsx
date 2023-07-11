2b438a44bd254b2822b1c2c3469e2ac97f562d9d
Ivan <ivan_leong_97@hotmail.com>
2023-07-11 21:49:18 +0800

    Convert Login page component into Form.Item

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -13,12 +13,14 @@ function LoginPage() {
       <div className="mb-12 text-center text-lg text-[#52525b]">
         Please enter your contact details to connect.
       </div>
-      <div className="mx-auto flex max-w-lg flex-col gap-2 rounded-2xl bg-white p-6 shadow-xl">
-        <Form layout="vertical">
+      <div className="mx-auto flex max-w-lg flex-col rounded-2xl bg-white p-6 shadow-xl">
+        <Form layout="vertical" requiredMark={false}>
           <Form.Item
             label="User name/ email address"
             name="username"
-            rules={[{ message: 'Please input your username!', required: true }]}
+            rules={[
+              { message: 'Please enter account information', required: true },
+            ]}
           >
             <Input placeholder="User name/ Email Address" />
           </Form.Item>
@@ -26,32 +28,67 @@ function LoginPage() {
           <Form.Item
             label="Password"
             name="password"
-            rules={[{ message: 'Please input your password!', required: true }]}
+            rules={[
+              { message: 'Please enter account information', required: true },
+            ]}
           >
             <Input.Password placeholder="Password" />
           </Form.Item>
-        </Form>
 
-        <div className="mb-4 flex flex-row justify-between text-sm">
-          <Checkbox>
-            Accept{' '}
-            <a className="text-[#2d5698]" href="#" rel="noreferrer">
-              Privacy policy
+          <Form.Item>
+            <Form.Item
+              name="policy"
+              noStyle
+              rules={[
+                {
+                  validator: (_, value) =>
+                    value
+                      ? Promise.resolve()
+                      : Promise.reject(
+                          new Error(
+                            'Please confirm that you have read and understood the Privacy Policy',
+                          ),
+                        ),
+                },
+              ]}
+              valuePropName="checked"
+            >
+              <Checkbox>
+                Accept{' '}
+                <a
+                  className="text-[#2d5698]"
+                  href="https://www.skillup4u.com/privacy-policy"
+                  rel="noreferrer"
+                  target="_blank"
+                >
+                  Privacy policy
+                </a>
+              </Checkbox>
+            </Form.Item>
+            <span className="float-right">
+              <a className="contents text-[#2d5698]" href="#">
+                Forgot password?
+              </a>
+            </span>
+          </Form.Item>
+
+          <Form.Item>
+            <Button
+              className="w-full justify-center text-base"
+              htmlType="submit"
+              type="primary"
+            >
+              Sign In
+            </Button>
+          </Form.Item>
+
+          <div className="mb-12 text-center text-sm text-[#71717a]">
+            {"Don't have an account? "}
+            <a className="contents font-semibold text-[#2d5698]" href="#">
+              Sign up
             </a>
-          </Checkbox>
-          <a className="contents text-[#2d5698]" href="#">
-            Forgot password?
-          </a>
-        </div>
-        <Button className="mb-4 h-16 justify-center text-base" type="primary">
-          Sign In
-        </Button>
-        <div className="mb-12 text-center text-sm text-[#71717a]">
-          {"Don't have an account? "}
-          <a className="contents font-semibold text-[#2d5698]" href="#">
-            Sign up
-          </a>
-        </div>
+          </div>
+        </Form>
       </div>
     </div>
   );
